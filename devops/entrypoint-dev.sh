c917974c33ab9829c47359ab00d505ed197b490a
BoringMind <super.d3280@ya.ru>
2023-07-01 20:22:53 +0400

    docs: added a reminder to not use --preload argument in Gunicorn

--- devops/entrypoint-dev.sh
+++ devops/entrypoint-dev.sh
@@ -15,7 +15,10 @@ python3 manage.py collectstatic --noinput;
 # Create superuser
 #python3 manage.py initadmin;
 
-# Run server
+# Run Gunicorn server
+# Preload argument will not work here. Since Django will retrieve secrets on it's
+# initialization. Django will not be re-initialized after Gunicorn restart if
+# --preload argument will be provided.
 gunicorn gunicorn_reload.wsgi:application --bind 0.0.0.0:80 --workers=4 --timeout=120 --limit-request-line=0 -p /opt/gunicorn_master.pid;
 
 # Evaluating passed command:
